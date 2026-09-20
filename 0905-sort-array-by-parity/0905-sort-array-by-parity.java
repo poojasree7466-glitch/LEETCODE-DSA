a953c2db3fab4920a1532bc1c3e6fec1fd0ddb8a
poojasree7466-glitch <poojasree7466@gmail.com>
2026-09-20 18:32:57 +0530

    Time: 0 ms (100%), Space: 46.7 MB (84.56%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int l=0;
+        int r=nums.length-1;
+        while(l<r)
+        {
+            if(nums[l]%2!=0&&nums[r]%2==0)
+            {
+                int temp=nums[l];
+                nums[l]=nums[r];
+                nums[r]=temp;
+                l++;
+                r--;
+            }
+            else if(nums[l]%2==0)
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+        return nums;
+    }
+}
